243c5774a0a303532c0da65e7ddc42f45c1337fe
kudinov <s.kudinov@list.ru>
2024-07-11 19:04:41 +0300

    nb update

--- my_module/__init__.py
+++ my_module/__init__.py
@@ -11,4 +11,23 @@ class MyTransformer(BaseEstimator, TransformerMixin):
     def transform(self, X, y=None):
         # Преобразуем столбец MSSubClass в строковый тип
         X['MSSubClass'] = X['MSSubClass'].astype(str)
+        X['OverallCond'] = X['OverallCond'].astype(str)
+        X['YrSold'] = X['YrSold'].astype(str)
+        X['MoSold'] = X['MoSold'].astype(str)
+        X['TotalSF'] = X['TotalBsmtSF'] + X['1stFlrSF'] + X['2ndFlrSF']
+        return X
+
+
+class GroupMedianImputer(BaseEstimator, TransformerMixin):
+    def __init__(self, groupby_col, target_col):
+        self.groupby_col = groupby_col
+        self.target_col = target_col
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        X = X.copy()
+        X[self.target_col] = X.groupby(self.groupby_col)[self.target_col].transform(
+            lambda x: x.fillna(x.median()))
         return X

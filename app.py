fc7c3fbaa3b7c3a3fa4ffc41d9523f3bdc010140
kudinov <s.kudinov@list.ru>
2024-07-12 12:28:52 +0300

    app update

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from my_module import MyTransformer, GroupMedianImputer, SomeCustomShit
 sklearn.set_config(transform_output="pandas")
 
 cb_pipeline = joblib.load('models/cb_pipeline.pkl')
+st_pipeline = joblib.load('models/stacking_pipeline.pkl')
 
 st.title('Real estate kaggle')
 st.caption('Серёжа²')
@@ -18,18 +19,20 @@ st.divider()
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    st.markdown('Pipeline с использованием модели **Catboost**')
+    st.markdown('Pipeline с использованием Stacking моделей **Catboost**, **SVR**, **GBoost**')
 with col3:
-    st.metric(label='RMSLE', value=0.12447)
+    st.metric(label='RMSLE', value=0.12262)
 
-st.image('reports/Screenshot 2024-07-12 at 10.16.21.png')
+st.image('reports/Screenshot 2024-07-12 at 12.16.36.png')
 
 with st.sidebar:
     st.write('Панель управления')
     file = st.file_uploader(label='Грузи свой датасет, бро', type='csv')
     if file:
         df = pd.read_csv(file)
-        answer = np.exp(cb_pipeline.predict(df))
+        answer0 = np.exp(cb_pipeline.predict(df))
+        answer1 = np.exp(st_pipeline.predict(df))
+        answer = 0.5 * answer0 + 0.5 * answer1
         result_df = pd.DataFrame({
             'Id': df['Id'],
             'SalePrice': answer
